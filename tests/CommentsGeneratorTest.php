60b0b045ef9749c5a9a05487da4fa80327a1fc1f
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-09-03 13:38:18 +0300

    Added comments generator tests

---
+++ tests/CommentsGeneratorTest.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types = 1);
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>.
+ * on 03.09.16 at 12:04
+ */
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+use samsonphp\generator\CommentsGenerator;
+
+/**
+ * Class GenericGeneratorTest
+ *
+ * @author Vitaly Egorov <egorov@samsonos.com>
+ */
+class CommentsGeneratorTest extends TestCase
+{
+    /** @var CommentsGenerator */
+    protected $generator;
+
+    public function setUp()
+    {
+        $this->generator = new CommentsGenerator();
+    }
+
+    public function testMultiLineComment()
+    {
+        $generated = $this->generator
+                ->defLine('Test comment line')
+                ->defLine('Test comment line2')
+                ->code();
+
+        $expected = <<<PHP
+/**
+ * Test comment line
+ * Test comment line2
+ */
+PHP;
+
+        static::assertEquals($expected, $generated);
+    }
+
+    public function testSingleLineComment()
+    {
+        $generated = $this->generator
+            ->defLine('Test comment line')
+            ->code();
+
+        $expected = <<<PHP
+/** Test comment line */
+PHP;
+
+        static::assertEquals($expected, $generated);
+    }
+}
